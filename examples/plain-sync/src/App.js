f1f6c2c7438f382708f3256f3f8a7c4fb7d20db2
Teimur Gasanov <teymurgg321@gmail.com>
2018-08-07 09:28:17 +0600

    Update example's routes' paths

--- examples/plain-sync/src/App.js
+++ examples/plain-sync/src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { PLAIN_SYNC_PATH } from ".";
 import PlainSyncDemo from "./PlainSyncDemo";
 import Main from "./Main";
 
 class App extends Component {
   render() {
+    const { PUBLIC_URL } = process.env;
     return (
-        <div>
-          <Route exact path="/" component={Main} />
-          <Route path={PLAIN_SYNC_PATH} component={PlainSyncDemo} />
-        </div>
+        <Switch>
+          <Route exact path={PUBLIC_URL} component={Main} />
+          <Route path={`${PUBLIC_URL}${PLAIN_SYNC_PATH}`} component={PlainSyncDemo} />
+        </Switch>
     );
   }
 }
